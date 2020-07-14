1f8d808164ea98f16cbd3cb7214515941829cdbe
DevLimk1 <kyenwon2@naver.com>
2020-07-14 20:00:28 +0900

    update 200714 implement main tag sections frame, footer

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+  let bars = document.querySelector(".bars");
+  let close=document.querySelector(".close");
+  bars.onclick = openSlideMenu;
+  close.onclick=closeSlideMenu;
 
-// Menu toggle-button
-$(document).ready(function(){
-    $(".menu-icon").on("click",function(){
-        $("nav ul").toggleClass("showing");
-    })
-})
+  function openSlideMenu() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+    document.querySelector(".fa-bars").classList.add("d-none");
+  }
 
-//Scrolling Effect
-
-$(window).on("scroll",function(){
-    if($(window).scrollTop()){
-        $('nav').addClass('black');
-    }else{
-        $('nav').removeClass('black');
-    }
-})
+  function closeSlideMenu() {
+    document.getElementById("menu").style.width = "0px";
+    document.getElementById("content").style.marginLeft = "0px";
+    document.querySelector(".fa-bars").classList.remove("d-none");
+  }
+});
