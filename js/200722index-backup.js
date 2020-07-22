25d116a5299c0a13b2497fa0cdd814eb1cb9474a
DevLimk1 <kyenwon2@naver.com>
2020-07-22 16:29:52 +0900

    update 200722 태그네임에 맞는 유튜브 영상 가져오기

--- js/200722index-backup.js
+++ js/200722index-backup.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
+ 
   // *********Sidebar Menu**********
-
+ 
   let bars = document.querySelector(".bars");
   let close = document.querySelector(".close");
   bars.onclick = openSlideMenu;
@@ -18,76 +19,23 @@ window.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".fa-bars").classList.remove("d-none");
   }
 
-  //  ************ tags section ****************
-
-  // $("#tags-section").on("click", function (e) {
-  //   let elem = e.target;
-  //   let txt = "";
-
-  //   if ($(elem).text() === "#" || $(elem).prop('tagName')==='I') {
-  //     elem = elem.nextElementSibling; 
-  //     txt = elem.innerText;
-  //   } else if ($(elem).attr("class") === "tag")
-  //     txt = $(elem).children()[1].innerText;
-  //   else if ($(elem).attr("class") === "text") // get className
-  //     txt = $(elem).text();
-  //   else return;
-
-  //   console.log(txt);
-  //   show(txt);
-  // });
-
-  // function show(txt){
-      
-  // }
-
   // *************** videos section ***************
   const apikey = "AIzaSyCGHtbofr5C_j5YRooXlsqljlb6WYp1Lqc";
   const channelId = "UCLG1XzhSPuuJ6hqaHhQaFkA";
   const videoChannel = document.querySelector("#video-channel");
   // const videoContainer = document.querySelector("#video-container");
   const thumsContainer = document.querySelector(".thums-container");
-  
+
   const channelEndpoint = `https://www.googleapis.com/youtube/v3/channels?key=${apikey}&id=${channelId}&part=snippet,contentDetails,statistics`;
-
-  $("#tags-section").on("click", function (e) {
-    let elem = e.target;
-    let txt = "";
-
-    if ($(elem).text() === "#" || $(elem).prop('tagName')==='I') {
-      elem = elem.nextElementSibling; 
-      txt = elem.innerText;
-    } else if ($(elem).attr("class") === "tag")
-      txt = $(elem).children()[1].innerText;
-    else if ($(elem).attr("class") === "text") // get className
-      txt = $(elem).text();
-    else return;
-
-    fetch(channelEndpoint)
-    .then((res) => res.json())
-    .then((data) => {
-      // console.log(data);
-      // showChannel(data);
-      let maxResults = 24;
-      const playlistId = data.items[0].contentDetails.relatedPlaylists.uploads;
-      requestPlayList(playlistId, maxResults,txt);
-    });
-
-    // show(txt);
-  });
-
-  // function show(txt){
-      
-  // }
 
   fetch(channelEndpoint)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data);
+      console.log(data);
       // showChannel(data);
       let maxResults = 24;
       const playlistId = data.items[0].contentDetails.relatedPlaylists.uploads;
-      requestPlayList(playlistId, maxResults);
+      requestPlayList(playlistId,maxResults);
     });
 
   // function showChannel(data) {
@@ -131,16 +79,16 @@ window.addEventListener("DOMContentLoaded", function () {
   //   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   // }
 
-  function requestPlayList(playlistId, maxResults,txt) {
+  function requestPlayList(playlistId,maxResults) {
     const playlistURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${apikey}&playlistId=${playlistId}&part=snippet&maxResults=${maxResults}`;
 
     fetch(playlistURL)
       .then((res) => res.json())
-      .then((data) => loadVideo(data,txt));
+      .then((data) => loadVideo(data));
   }
 
-  function loadVideo(data,txt) {
-    // console.log(data);
+  function loadVideo(data) {
+    console.log(data);
     const playListItems = data.items;
 
     if (playListItems) {
@@ -150,50 +98,26 @@ window.addEventListener("DOMContentLoaded", function () {
         const videoId = item.snippet.resourceId.videoId;
         const thumbnailsUrl = item.snippet.thumbnails.standard.url;
         const channelTitle = item.snippet.channelTitle;
-        const title = item.snippet.title;
+        const title=item.snippet.title;
         const thumbnailsDescription = item.snippet.description;
-        // console.log(thumbnailsUrl);
+        console.log(thumbnailsUrl);
         // console.log(thumbnailsDescription);
-        // let txt='팔';
-        if(!txt)
-          txt='전체';
 
-        if(title.includes(txt)){
-          
-          output += `
-          <div class="thums-box">
-          <div class="title text-center">${channelTitle}</div>
-          <div class="imgBox">
-              <img
-                  src="${thumbnailsUrl}">
-                  <div class="details">
-                  <div class="content">
-                      <h2>${title}</h2>
-                  </div>
-              </div>
-          </div>
-         
-          </div>
-              `;
-        }else if(txt==='전체'){
-          output += `
-          <div class="thums-box">
-          <div class="title text-center">${channelTitle}</div>
-          <div class="imgBox">
-              <img
-                  src="${thumbnailsUrl}">
-                  <div class="details">
-                  <div class="content">
-                      <h2>${title}</h2>
-                  </div>
-              </div>
-          </div>
-         
-          </div>
-              `;
-        }
-          
-
+        output += `
+        <div class="thums-box">
+        <div class="title text-center">${channelTitle}</div>
+        <div class="imgBox">
+            <img
+                src="${thumbnailsUrl}">
+                <div class="details">
+                <div class="content">
+                    <h2>${title}</h2>
+                </div>
+            </div>
+        </div>
+       
+        </div>
+            `;
       });
       //   output += `
       //             <div class="col-lg-4 col-md-6 mb-4">
@@ -209,4 +133,8 @@ window.addEventListener("DOMContentLoaded", function () {
       thumsContainer.innerHTML = "sorry, No videos uploaded!";
     }
   }
+
+
+  
+  
 });
